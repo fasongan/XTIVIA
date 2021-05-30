0cec201236ddb59b6f0988e793e670851c1a2e44
root <fasongan@gmail.com>
2021-05-30 07:44:09 -0500

    created bash script to cp files w/ arguemtns

---
+++ home/ansible/Documents/xtivia/cpscripts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+threshold=$1
+source_s3=$2
+dest_s3=$3
+
+AWS_ID=
+AWS_key=
+
+#list files in s3
+aws s3 ls $source_s3 --recursive --human-readable --summarize >> source_files
+
+for file in $(cat source_files) ;
+do  
+size=`cat file | awk -F " " '{ print $3 }'`
+if [ $threshold -gt 0 ] ; then
+
+path=`cat file | awk -F " " '{ print $5 }'`
+#copy the files to destination s3 bucket
+aws s3 cp $source_s3/$path $dest_s3
+
+fi
+
